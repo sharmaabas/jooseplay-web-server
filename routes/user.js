0aec7ac1e6f42d818ec2c576ed4145e1901b9d16
sharmaabas <sharma.abas.10@gmail.com>
2023-04-10 17:56:02 +0300

    tenth commit

--- routes/user.js
+++ routes/user.js
@@ -7,12 +7,12 @@ const router = express.Router()
 router.get(
 	'/users',
 	asyncHandler(async (req, res, next) => {
-		/*const { page } = req.query ?? 0
+		const { page } = req.query ?? 0
 		const { limit } = req.query ?? 20
-		const offset = page * limit*/
+		const offset = page * limit
 		const { rows } = await pool.query(
-			`select avatar,first_name,last_name,username from songappusers`/* offset$1 limit $2;`,*/
-			/*[offset, limit]*/
+			`select avatar,first_name,last_name,username from songappusers offset$1 limit $2;`,
+			[offset, limit]
 		)
 
 		res.send({ results: rows })
